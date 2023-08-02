4659a3e7e14f6b67e306c9e8a2f1bd315032920e
havaeng <havang@me.com>
2023-08-02 12:13:39 +0200

    Book inventory implemented!

--- LearningKotlin/src/main/kotlin/BooksCLI/Book.kt
+++ LearningKotlin/src/main/kotlin/BooksCLI/Book.kt
@@ -1,25 +1,76 @@
-package BooksCLI
+package BookInventory
 
-class Book {
-    val ISBN: Int? = null
-    val title : String? = null
-    val author : String? = null
-    val yearOfPublication : String? = null
-    val quantity : Int = 0
+/**
+ * A very simple CLI with objects from one class.
+ */
 
-    fun addBook(){
+class Book() {
+    var ISBN: String? = null
+    var title: String? = null
+    var author: String? = null
+    var yearOfPublication: String? = null
 
+    constructor(ISBN: String, title: String, author: String, year: String) : this() {
+        this.ISBN = ISBN
+        this.title = title
+        this.author = author
+        this.yearOfPublication = year
     }
+}
 
-    fun displayDetails(){
+fun addBook(ISBN: String, title: String, author: String, year: String): Book {
+    return Book(ISBN, title, author, year)
+}
 
-    }
+fun main(args: Array<String>) {
+    val bookInventory: ArrayList<Book> = ArrayList()
 
-    fun updateQuantity(){
+    do {
 
-    }
+        println("\nMenu:\n1. Add book\n2. Display details\n3. Exit program")
 
-    fun main(args: Array<String>) {
+        var choice: Int? = readlnOrNull()?.toIntOrNull()
 
-    }
+        when (choice) {
+            1 -> {
+                println("Adding new book")
+                println("ISBN: ")
+                var ISBN = readlnOrNull()
+                println("Title: ")
+                var title = readlnOrNull()
+                println("Author: ")
+                var author = readlnOrNull()
+                println("Year: ")
+                var year = readlnOrNull()
+                bookInventory.add(
+                    addBook(
+                        ISBN.toString(),
+                        title.toString(),
+                        author.toString(),
+                        year.toString()
+                    )
+                )
+            }
+            2 -> {
+                println("Enter ISBN of book you would like to see details for:")
+                var ISBN = readln()
+                var bookFound = false
+
+                for (book in bookInventory)
+                    if (book.ISBN == ISBN) {
+                        println("ISBN: ${book.ISBN}")
+                        println("Title: ${book.title}")
+                        println("Author: ${book.author}")
+                        println("Publication Year: ${book.yearOfPublication}")
+                        bookFound = true
+                        break
+                    }
+                if (bookFound == false)
+                    println("Book not found")
+            }
+            else -> {
+                println("Wrong input")
+            }
+        }
+    } while (choice != 3)
 }
